6292cd8fa409f59dc9102ddd89a854aed6d34f94
yashr775 <rajy29629@gmail.com>
2024-08-27 17:13:11 +0530

    Search dialogue completed

---
+++ Client/src/components/shared/UserItem.tsx
@@ -0,0 +1,68 @@
+import { ListItem, Stack, Avatar, Typography, IconButton } from "@mui/material";
+import { Add as AddIcon, Remove as RemoveIcon } from "@mui/icons-material";
+import React, { memo } from "react";
+
+interface UserItemPropTypes {
+  user: {
+    name: string;
+    _id: string;
+    avatar: string;
+  };
+  handler: (_id: string) => void;
+  handlerIsLoading: boolean;
+  isAdded?: boolean;
+}
+
+const UserItem = ({
+  user,
+  handler,
+  handlerIsLoading,
+  isAdded = false,
+}: UserItemPropTypes) => {
+  const { name, _id, avatar } = user;
+
+  return (
+    <ListItem>
+      <Stack
+        direction={"row"}
+        alignItems={"center"}
+        spacing={"1rem"}
+        width={"100%"}
+      >
+        <Avatar src={avatar} />
+
+        <Typography
+          variant="body1"
+          sx={{
+            flexGlow: 1,
+            display: "-webkit-box",
+            WebkitLineClamp: 1,
+            WebkitBoxOrient: "vertical",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            width: "100%",
+          }}
+        >
+          {name}
+        </Typography>
+
+        <IconButton
+          size="small"
+          sx={{
+            bgcolor: isAdded ? "error.main" : "primary.main",
+            color: "white",
+            "&:hover": {
+              bgcolor: isAdded ? "error.dark" : "primary.dark",
+            },
+          }}
+          onClick={() => handler(_id)}
+          disabled={handlerIsLoading}
+        >
+          {isAdded ? <RemoveIcon /> : <AddIcon />}
+        </IconButton>
+      </Stack>
+    </ListItem>
+  );
+};
+
+export default memo(UserItem);
--- Client/src/components/specific/Search.tsx
+++ Client/src/components/specific/Search.tsx
@@ -1,7 +1,69 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useInputValidation } from "6pp";
+import {
+  Dialog,
+  DialogTitle,
+  InputAdornment,
+  List,
+  ListItem,
+  ListItemText,
+  Stack,
+  TextField,
+} from "@mui/material";
 import React from "react";
+import { Search as SearchIcon } from "@mui/icons-material";
+import UserItem from "../shared/UserItem";
+
+// interface userType {
+//   name: string;
+//   email: string;
+// }
+
+const users = [1, 2, 3];
 
 const Search = () => {
-  return <div>Search</div>;
+  const search = useInputValidation("");
+
+  const addFriendHandler = (id: string) => {
+    console.log(id);
+  };
+
+  return (
+    <Dialog open>
+      <Stack p={"2rem"} direction={"column"} width={"25rem"}>
+        <DialogTitle textAlign={"center"}>Find People</DialogTitle>
+        <TextField
+          label=""
+          value={search.value}
+          onChange={search.changeHandler}
+          variant="outlined"
+          size="small"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />{" "}
+        <List>
+          {users.map((user) => (
+            <UserItem
+              user={{
+                name: "gfhhfgh",
+                _id: "5433",
+                avatar: "https://www.w3schools/howto/img_avatar.png",
+              }}
+              handler={function (_id: string): void {
+                throw new Error("Function not implemented.");
+              }}
+              handlerIsLoading={false}
+            />
+          ))}
+        </List>
+      </Stack>
+    </Dialog>
+  );
 };
 
 export default Search;
