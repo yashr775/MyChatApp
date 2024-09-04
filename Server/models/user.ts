f0d15386fcb96e0da089e1e5799912fae3b2926a
yashr775 <rajy29629@gmail.com>
2024-09-04 10:27:46 +0530

    Schema completed

---
+++ Server/models/user.ts
@@ -0,0 +1,40 @@
+import mongoose, { Schema , model,models } from "mongoose";
+
+
+const schema = new Schema({
+    name: {
+        type: String,
+        required: true,
+      },
+      bio: {
+        type: String,
+        required: true,
+      },
+      username: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: true,
+        select: false,
+      },
+      avatar: {
+        public_id: {
+          type: String,
+          required: true,
+        },
+        url: {
+          type: String,
+          required: true,
+        },
+      },
+    },
+    {
+      timestamps: true,
+    }
+)
+
+
+export const User = mongoose.models.User || model("User", schema);
